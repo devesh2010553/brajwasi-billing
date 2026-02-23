df281f2698fc6f03305e08c94dfe69454e226cae
devesh2010553 <133110094+devesh2010553@users.noreply.github.com>
2026-02-23 12:37:35 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,26 @@
-const CACHE_NAME = "brajwasi-cache-v1";
+const CACHE_NAME = "brajwasi-v2";
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "/",
-        "/static/style.css",
-        "/static/icons/icon-192.png",
-        "/static/icons/icon-512.png"
-      ]);
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
     })
   );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
